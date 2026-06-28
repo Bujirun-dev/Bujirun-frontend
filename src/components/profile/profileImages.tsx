9002cd94753ab3ad4b4bd152d8560f503612c208
parksungbin <sungbin931@gmail.com>
2026-06-28 12:27:45 +0900

    refactor: 프로필 이미지 상수 공통 파일로 분리

---
+++ src/components/profile/profileImages.tsx
@@ -0,0 +1,15 @@
+import profileHat from "@/assets/character/profile/profile-hat.png";
+import profileHeart from "@/assets/character/profile/profile-heart.png";
+import profileParty from "@/assets/character/profile/profile-party.png";
+import profileSleepy from "@/assets/character/profile/profile-sleepy.png";
+import profileSunglass from "@/assets/character/profile/profile-sunglass.png";
+import profileV from "@/assets/character/profile/profile-v.png";
+
+export const PROFILE_IMAGES = [
+  { id: 1, src: profileHat },
+  { id: 2, src: profileHeart },
+  { id: 3, src: profileParty },
+  { id: 4, src: profileSleepy },
+  { id: 5, src: profileSunglass },
+  { id: 6, src: profileV },
+];
