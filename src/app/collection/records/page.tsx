3ca777bd95fef746394a6f4dd98844ea1fca6dac
parksungbin <sungbin931@gmail.com>
2026-08-28 22:02:28 +0900

    style: prettier 버전 불일치로 인한 포맷 오류 수정

--- src/app/collection/records/page.tsx
+++ src/app/collection/records/page.tsx
@@ -70,7 +70,8 @@ export default function CollectionRecordsPage() {
     }, {});
 
     return Object.entries(count).sort(([, a], [, b]) => (b ?? 0) - (a ?? 0))[0]?.[0] as
-      Category | undefined;
+      | Category
+      | undefined;
   }, [collectedPlaces]);
 
   const records = useMemo(
