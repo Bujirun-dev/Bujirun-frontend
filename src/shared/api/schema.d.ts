c8c92ab4c2fe5f336abf005ca7fb46ab10d70181
Yujung Choi <yujung5548@naver.com>
2026-09-15 03:05:29 +0900

    fix: 카테고리 필드 수정 및 SpotSummary 타입 갱신

--- src/shared/api/schema.d.ts
+++ src/shared/api/schema.d.ts
@@ -1619,6 +1619,7 @@ export interface components {
             id?: string;
             name?: string;
             category?: string;
+            collectionCategory?: string;
             address?: string;
             lat?: number;
             lng?: number;
